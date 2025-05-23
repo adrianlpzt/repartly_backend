c16dd331ac9ea0a59b8ce1e5de60669473ad9119
adrian <adrianlopeztejera@gmail.com>
2025-05-23 20:33:29 +0200

    fixed cors 3

--- core/settings.py
+++ core/settings.py
@@ -53,7 +53,7 @@ REST_FRAMEWORK = {
     )
 }
 
-CORS_ALLOWED_ORIGINS = ["https://repartly.themonument.es"]
+CORS_ALLOWED_ORIGINS = ["http://repartly.themonument.es"]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
